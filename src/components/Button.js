f78e28f22ca293dba0671565157101a18c20c1a3
Juliet <rubin.juliet@gmail.com>
2020-02-09 23:05:20 -0700

    basic styling and onClick started

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,11 @@ import PropTypes from "prop-types"
 import classNames from 'classnames';
 import styles from "./calculator.module.css"
 
+// TODO: debounce
 
-const Button = (props) => (
-  <div className={classNames({[styles.button]: true, [styles.wide_button]: props.wide_button})}>{props.value} </div>
-  // <div className={classNames({[styles.button]: true})}>{props.value} </div>
-
+const Button = ({value, wide_button, onClick}) => (
+  <button onClick={onClick}
+    className={classNames({[styles.button]: true, [styles.wide_button]: wide_button})}>{value} </button>
 )
 
 Button.propTypes = {
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,51 @@
 import React from "react"
 
+import styles from "./calculator.module.css"
 import Button from "./Button"
 
-import styles from "./calculator.module.css"
 
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {equation: ""};
 
-const Calculator = () => (
-  <div className={styles.container}> 
-    <div className={styles.calculator}> 
+  }
+
+  handleClick() {
+    console.log("clicked");
+  }
+
+  render() {
+    return (
+      <div className={styles.calculator}> 
         <div className={styles.output_container}><div className={styles.output_text}>$1,000</div></div>
-        <Button value="C"></Button>
-        <Button value="+/="></Button>
-        <Button value="%"></Button>
-        <Button value="÷"></Button>
+        <Button onClick={this.handleClick} value="C"></Button>
+        <Button onClick={this.handleClick} value="+/="></Button>
+        <Button onClick={this.handleClick} value="%"></Button>
+        <Button onClick={this.handleClick} value="÷"></Button>
 
-        <Button value="7"></Button>
-        <Button value="8"></Button>
-        <Button value="9"></Button>
-        <Button value="x"></Button>
+        <Button onClick={this.handleClick} value="7"></Button>
+        <Button onClick={this.handleClick} value="8"></Button>
+        <Button onClick={this.handleClick} value="9"></Button>
+        <Button onClick={this.handleClick} value="x"></Button>
 
-        <Button value="4"></Button>
-        <Button value="5"></Button>
-        <Button value="6"></Button>
-        <Button value="-"></Button>
+        <Button onClick={this.handleClick} value="4"></Button>
+        <Button onClick={this.handleClick} value="5"></Button>
+        <Button onClick={this.handleClick} value="6"></Button>
+        <Button onClick={this.handleClick} value="-"></Button>
 
+        <Button onClick={this.handleClick} value="1"></Button>
+        <Button onClick={this.handleClick} value="2"></Button>
+        <Button onClick={this.handleClick} value="3"></Button>
+        <Button onClick={this.handleClick} value="+"></Button>
 
-        <Button value="1"></Button>
-        <Button value="2"></Button>
-        <Button value="3"></Button>
-        <Button value="+"></Button>
-
-
-        <Button wide_button={true} value="0"></Button>
-        <Button value="."></Button>
-        <Button value="="></Button>
-
+        <Button onClick={this.handleClick} wide_button={true} value="0"></Button>
+        <Button onClick={this.handleClick} value="."></Button>
+        <Button onClick={this.handleClick} value="="></Button>
     </div>
-  </div>
-)
+    );
+  }
+}
 
 export default Calculator
